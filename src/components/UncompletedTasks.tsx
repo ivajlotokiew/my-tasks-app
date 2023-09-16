5ad040fd56e27f3cc03e34e77117803db58e00c4
ivajlotokiew <ivajlo_nick@abv.bg>
2023-09-16 17:53:34 +0300

    show uncompleted tasks

--- src/components/UncompletedTasks.tsx
+++ src/components/UncompletedTasks.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import { useSelector } from "react-redux/es/hooks/useSelector"
+import { Task, getUncompletedTasks } from "../features/tasks/tasksSlice"
+import TaskItem from "./TaskItem"
 
 const UncompletedTasks = () => {
+    const completedTasks = useSelector(getUncompletedTasks)
+
     return (
-        <div>UncompletedTasks</div>
+        <div >
+            {completedTasks?.map((task: Task) =>
+                <div key={task.id}>
+                    <TaskItem task={task} />
+                </div>)}
+        </div>
     )
 }
 
