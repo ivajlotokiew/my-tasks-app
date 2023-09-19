84b4418ba6597e5bb75759d5e5cf40b7cb731028
ivajlotokiew <ivajlo_nick@abv.bg>
2023-09-19 21:01:22 +0300

    Search bar style

--- src/components/HorizontalBar.tsx
+++ src/components/HorizontalBar.tsx
@@ -2,7 +2,12 @@ import styles from './HorizontalBar.module.css'
 
 const HorizontalBar = () => {
     return (
-        <div>HorizontalBar</div>
+        <div style={{ marginTop: '30px' }}>
+            <form className={styles.form}>
+                <input type="search" placeholder="Search..." />
+                <button type="submit">Search</button>
+            </form>
+        </div>
     )
 }
 
