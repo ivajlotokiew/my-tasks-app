3d3af0d2a596cf665293ad53befbcbef71d7f530
ivajlotokiew <ivajlo_nick@abv.bg>
2023-09-20 20:02:13 +0300

    Navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,13 +22,15 @@ const Navbar = () => {
     return (
         <div className={styles.wrapper}>
             <h3>TO-DO LIST</h3>
-            <TasksModal nameForm="Add a task" modalIsOpen={showModal} setIsOpen={setShowModal}>
-                <CustomButton
-                    style={{ margin: '0 15px', padding: '15px 0', background: 'rgb(91, 33, 182)' }}
-                    onClick={handleShowModalEvent}>
-                    Add new task
-                </CustomButton>
-            </TasksModal>
+            <div style={{ marginBottom: '15px' }}>
+                <TasksModal nameForm="Add a task" modalIsOpen={showModal} setIsOpen={setShowModal}>
+                    <CustomButton
+                        style={{ width: '-webkit-fill-available', margin: '0 15px', padding: '15px 0', background: 'rgb(91, 33, 182)' }}
+                        onClick={handleShowModalEvent}>
+                        Add new task
+                    </CustomButton>
+                </TasksModal>
+            </div>
             {navBarMenu.map((item) =>
                 <div className={styles.navLink} key={item.target}>
                     <NavLink to={item.target} >
