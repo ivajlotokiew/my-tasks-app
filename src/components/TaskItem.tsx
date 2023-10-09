bfb00919736e722ac5c985e147533e034e33248b
ivajlotokiew <ivajlo_nick@abv.bg>
2023-10-09 20:10:19 +0300

    style improvements

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -47,74 +47,78 @@ const TaskItem = ({ task, reload, stateTasks }: Props) => {
 
     return (
         <>
-            <NavLink to={`/dir/${getTaskDirectory?.id}/tasks`}>
-                <div className={styles.taskDirectory}>{getTaskDirectory?.title}</div>
-                <div className={styles.directoryTaskLabelPopup}>
-                    {getTaskDirectory?.title}
-                </div>
-            </NavLink>
-            <h4>{task.title}</h4>
-            <h5>{task.description}</h5>
-            <div className={styles.created}>
-                <img src='/calendar.svg' alt='mySvgImage' width="25" />
-                {task.date}
-            </div>
-            <hr className={styles.lineSeparator} />
-            <LoadingOverlay
-                active={loading}
-                spinner
-                styles={{
-                    spinner: (base) => ({
-                        ...base,
-                        '& svg circle': {
-                            stroke: 'rgba(255, 0, 0, 0.5)'
-                        }
-                    }),
-                }}
-            >
-                <div className={styles.actions}>
-                    <CustomButton onClick={toggleCompletedTask}>
-                        {task.completed ? 'completed' : 'uncompleted'}
-                    </CustomButton>
-                    <div className={styles.completedTaskLabelPopup}>
-                        {task.completed ? 'Unmark ' : 'Mark '} as completed
+            <div>
+                <NavLink to={`/dir/${getTaskDirectory?.id}/tasks`}>
+                    <div className={styles.taskDirectory}>{getTaskDirectory?.title}</div>
+                    <div className={styles.directoryTaskLabelPopup}>
+                        {getTaskDirectory?.title}
                     </div>
-                    <div className={styles.hdgLabelInfo} style={{ display: "flex", alignItems: "center" }}>
-                        <img src={task.important ? '/star-red.svg' : '/star-white.svg'}
-                            className={styles.starIcon}
-                            role="button"
-                            alt='mySvgImage'
-                            width="22"
-                            onClick={toggleImportantTask}
-                            style={{ cursor: "pointer", marginLeft: "10px" }} />
-                        <div className={styles.importantTaskLabelPopup}>
-                            {task.important ? 'Unmark ' : 'Mark '} as important
+                </NavLink>
+                <h4>{task.title}</h4>
+                <h5>{task.description}</h5>
+            </div>
+            <div>
+                <div className={styles.created}>
+                    <img src='/calendar.svg' alt='mySvgImage' width="25" />
+                    {task.date}
+                </div>
+                <hr className={styles.lineSeparator} />
+                <LoadingOverlay
+                    active={loading}
+                    spinner
+                    styles={{
+                        spinner: (base) => ({
+                            ...base,
+                            '& svg circle': {
+                                stroke: 'rgba(255, 0, 0, 0.5)'
+                            }
+                        }),
+                    }}
+                >
+                    <div className={styles.actions}>
+                        <CustomButton onClick={toggleCompletedTask}>
+                            {task.completed ? 'completed' : 'uncompleted'}
+                        </CustomButton>
+                        <div className={styles.completedTaskLabelPopup}>
+                            {task.completed ? 'Unmark ' : 'Mark '} as completed
                         </div>
-                        <img src='/trash-white.svg'
-                            className={styles.trashIcon}
-                            role="button"
-                            alt='mySvgImage'
-                            width="22"
-                            onClick={handleShowDeleteModalEvent}
-                            style={{ cursor: "pointer", marginLeft: "10px" }} />
-                        <div className={styles.deleteTaskLabelPopup}>
-                            Delete task
-                        </div>
-                        <TaskModal task={task} modalIsOpen={showModal} directory={getTaskDirectory} stateTasks={stateTasks} setIsOpen={setShowModal} nameForm={'Edit task'}>
-                            <img src='/three-dots-vertical-white.svg'
-                                className={styles.threeDotsIcon}
+                        <div className={styles.hdgLabelInfo} style={{ display: "flex", alignItems: "center" }}>
+                            <img src={task.important ? '/star-red.svg' : '/star-white.svg'}
+                                className={styles.starIcon}
                                 role="button"
                                 alt='mySvgImage'
                                 width="22"
-                                onClick={handleShowModalEvent}
+                                onClick={toggleImportantTask}
                                 style={{ cursor: "pointer", marginLeft: "10px" }} />
-                        </TaskModal>
-                        <div className={styles.editTaskLabelPopup}>
-                            Edit task
+                            <div className={styles.importantTaskLabelPopup}>
+                                {task.important ? 'Unmark ' : 'Mark '} as important
+                            </div>
+                            <img src='/trash-white.svg'
+                                className={styles.trashIcon}
+                                role="button"
+                                alt='mySvgImage'
+                                width="22"
+                                onClick={handleShowDeleteModalEvent}
+                                style={{ cursor: "pointer", marginLeft: "10px" }} />
+                            <div className={styles.deleteTaskLabelPopup}>
+                                Delete task
+                            </div>
+                            <TaskModal task={task} modalIsOpen={showModal} directory={getTaskDirectory} stateTasks={stateTasks} setIsOpen={setShowModal} nameForm={'Edit task'}>
+                                <img src='/three-dots-vertical-white.svg'
+                                    className={styles.threeDotsIcon}
+                                    role="button"
+                                    alt='mySvgImage'
+                                    width="22"
+                                    onClick={handleShowModalEvent}
+                                    style={{ cursor: "pointer", marginLeft: "10px" }} />
+                            </TaskModal>
+                            <div className={styles.editTaskLabelPopup}>
+                                Edit task
+                            </div>
                         </div>
                     </div>
-                </div>
-            </LoadingOverlay>
+                </LoadingOverlay>
+            </div>
             <DeleteItemModal description="This task will be deleted permanently." itemName="task" modalIsOpen={showDeleteModal} item={task} setIsOpen={setShowDeleteModal} />
 
         </>
