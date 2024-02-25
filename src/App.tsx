a1c1a50ef2d672c46f0539edbf29b063068d020a
ivajlotokiew <ivajlo_nick@abv.bg>
2024-02-25 15:30:32 +0200

    Change before merge

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import SearchedTask from './components/pages/SearchedTask';
 
 function App() {
   return (
+    //Changes before merge
     <Routes>
       <Route path="/" element={
         <PrivateRoute>
